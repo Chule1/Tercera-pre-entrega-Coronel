cb0d94107648abc04e1c94e0bc7f9bc2f8116e74
Matías A. Coronel <matiasarielcoronel@gmail.com>
2023-06-25 15:16:03 -0300

    Primeros templates

--- Proyecto/App1/admin.py
+++ Proyecto/App1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Clientes)
+admin.site.register(Vendedores)
+admin.site.register(Productos)
+admin.site.register(RecursosHumanos)
--- Proyecto/App1/views.py
+++ Proyecto/App1/views.py
@@ -1,17 +1,19 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
 # Create your views here.
 def inicio(request):
-    return render(request, "App1/inicio.html")
+    return render(request, "inicio.html")
 
 def Clientes(request):
-    return render(request, "App1/Clientes.html")
+    return render(request, "Clientes.html")
 
 def Vendedores(request):
-    return render(request, "App1/Vendedores.html")
+    return render(request, "Vendedores.html")
 
 def Productos(request):
-    return render(request, "App1/Productos.html")
+    return render(request, "Productos.html")
 
 def RecursosHumanos(request):
-    return render(request, "App1/RecursosHumanos.html")
+    return render(request, "RecursosHumanos.html")
